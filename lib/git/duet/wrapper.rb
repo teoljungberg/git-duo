24b2105e2b47ac6018d73a0bd33d3f4c9ad0550f
Teo Ljungberg <teo.ljungberg@gmail.com>
2013-10-05 17:08:59 +0200

    Basic git wrapper

---
+++ lib/git/duet/wrapper.rb
@@ -0,0 +1,37 @@
+module Git
+  module Duet
+    class Wrapper
+      class << self
+        def email_format
+          config 'git-duet.email-format'
+        end
+
+        def email_format= format
+          config "git-duet.email-format '#{format}'"
+        end
+
+        def user_email
+          config "user.email"
+        end
+
+        def user_email= email
+          config "user.email '#{email}'"
+        end
+
+        def user_name
+          config "user.name"
+        end
+
+        def user_name= name
+          config "user.name '#{name}'"
+        end
+
+        private
+
+        def config arg
+          `git config #{arg}`.strip
+        end
+      end
+    end
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,5 @@
+require 'git/duet/wrapper'
+require 'ostruct'
 require 'pry'
 
 RSpec.configure do |config|
@@ -5,6 +7,12 @@ RSpec.configure do |config|
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
   config.order = 'random'
+
+  config.before { Git::Duet::Wrapper.stub(:email_format) { 'dev@mynewsdesk.com' } }
+  config.before { Git::Duet::Wrapper.stub(:set_email) { author.email } }
+  config.before { Git::Duet::Wrapper.stub(:email_name) { author.name } }
+  config.before { Git::Duet::Wrapper.stub(:set_email=) { author.email } }
+  config.before { Git::Duet::Wrapper.stub(:email_name=) { author.name } }
 end
 
 def author
@@ -12,4 +20,17 @@ def author
     name: 'Teo Ljungberg',
     email: 'teo@mynewsdesk.com',
     full: 'Teo Ljungberg <teo@mynewsdesk.com>'
+end
+
+def authors
+  [
+    OpenStruct.new(key: 'teo',
+                   name: 'Teo Ljungberg',
+                   email: 'teo@mynewsdesk.com',
+                   full: 'Teo Ljungberg <teo@mynewsdesk.com>'),
+    OpenStruct.new(key: 'billskog',
+                   name: 'David Billskog',
+                   email: 'david.billskog@mynewsdesk.com',
+                   full: 'David Billskog <david.billskog@mynewsdesk.com>')
+  ]
 end
