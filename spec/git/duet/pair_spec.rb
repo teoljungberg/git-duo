cb7279a55ff13b793166ae3cec49ff86b1713b81
Teo Ljungberg <teo.ljungberg@gmail.com>
2013-10-27 14:17:25 +0100

    Split up test to follow 1 assertion per it block

--- spec/git/duet/pair_spec.rb
+++ spec/git/duet/pair_spec.rb
@@ -6,10 +6,23 @@ describe Git::Duet::Pair do
   subject { described_class.new authors }
 
   describe '#set' do
-    it 'sets up the pair name and email' do
-      Git::Duet::Repo.any_instance.stub(:email) { 'dev@mynewsdesk.com' }
+    before { Git::Duet::Repo.any_instance.stub(:email) { 'dev@mynewsdesk.com' } }
+
+    it 'sets up the pair name' do
       expect(subject).to receive(:set_pair_name) { pair_name }
+
+      subject.set
+    end
+
+    it 'sets up pair email' do
       expect(subject).to receive(:set_pair_email) { pair_email }
+
+      subject.set
+    end
+
+    it 'displays the current committer' do
+      subject.stub(:set_pair_name) { pair_name }
+      subject.stub(:current_committer) { pair_email }
 
       expect(subject.set).to eq 'David Billskog and Teo Ljungberg <dev+billskog+teo@mynewsdesk.com>'
     end
