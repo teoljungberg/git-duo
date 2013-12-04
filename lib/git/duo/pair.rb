5a862490bf166e13ea2977b28d6819aa07599dee
Teo Ljungberg <teo.ljungberg@gmail.com>
2013-12-04 17:59:35 +0100

    Pair#save to save the current pair to git
    Dependency inject Wrapper for testing

--- lib/git/duo/pair.rb
+++ lib/git/duo/pair.rb
@@ -1,10 +1,15 @@
+require 'git/duo/wrapper'
+
 module Git::Duo
   class Pair
     attr_reader :users, :base_email
 
-    def initialize(users, base_email)
+    def initialize(users, base_email, wrapper = nil)
       @users = users.sort_by {|user| user.key }
       @base_email = base_email
+      @wrapper = wrapper || Git::Duo::Wrapper.new(
+        File.join(Dir.pwd, '.git')
+      )
     end
 
     def name
@@ -19,5 +24,21 @@ module Git::Duo
       "#{name} <#{email}>"
     end
     alias_method :inspect, :to_s
+
+    def save
+      save_pair_name
+      save_pair_email
+    end
+
+    private
+    attr_reader :wrapper
+
+    def save_pair_name
+      wrapper.config "user.name '#{name}'"
+    end
+
+    def save_pair_email
+      wrapper.config "user.email '#{email}'"
+    end
   end
 end
--- test/git/duo/pair_test.rb
+++ test/git/duo/pair_test.rb
@@ -25,5 +25,15 @@ module Git::Duo
     def test_inspect_is_to_s
       assert_equal pair.to_s, pair.inspect
     end
+
+    def test_save
+      wrapper = Minitest::Mock.new.
+        expect(:config, :nope, ["user.name 'Alfred Pennyworth + Bruce Wayne'"]).
+        expect(:config, :nope, ["user.email 'board+alfred+bruce@gotham.com'"])
+      pair = Pair.new dummy_users, "board+%names@gotham.com", wrapper
+
+      pair.save
+      wrapper.verify
+    end
   end
 end
