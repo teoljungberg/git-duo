4ff3edfc7f70b5ff192a75daf1baf955c7f1afd3
Teo Ljungberg <teo.ljungberg@gmail.com>
2013-12-29 18:51:18 +0100

    Test Array#where's case insensitivity separately

--- test/git/duo/core_extensions/array_test.rb
+++ test/git/duo/core_extensions/array_test.rb
@@ -3,8 +3,12 @@ require_relative '../../../test_helper.rb'
 module Git::Duo::CoreExtensions
   class ArrayTest < Minitest::Test
     def test_where_can_find_any_attribute
+      refute_empty alfred_and_bruce.where(key: 'bruce')
+      refute_empty alfred_and_bruce.where(name: 'alfred')
+    end
+
+    def test_where_is_case_insensitive
       refute_empty alfred_and_bruce.where(key: 'brUCE')
-      refute_empty alfred_and_bruce.where(name: 'Alfred')
     end
 
     def test_where_returns_empty_array_if_nothing_is_matched
