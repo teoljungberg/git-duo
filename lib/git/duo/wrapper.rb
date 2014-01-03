50bf52418b66dc333d3ac473f7773b0ae682a1d5
Teo Ljungberg <teo.ljungberg@gmail.com>
2014-01-03 19:03:57 +0100

    Extract magic pipe to constant

--- lib/git/duo/wrapper.rb
+++ lib/git/duo/wrapper.rb
@@ -1,12 +1,14 @@
 module Git
   module Duo
     class Wrapper
+      PIPE_STDOUT_TO_STDERR = '2>&1'
+
       def initialize(directory)
         @directory = directory
       end
 
       def config(args)
-        `git --git-dir=#{@directory} config #{args} 2>&1`.split("\n")
+        `git --git-dir=#{@directory} config #{args} #{PIPE_STDOUT_TO_STDERR}`.split("\n")
       end
     end
   end
